c38d72606a669dbc46ec01ec412755a1cfdca355
syt123450 <421537891@qq.com>
2017-11-16 01:40:57 -0800

    init project

---
+++ src/main/java/com/ticketSolder/TicketsolderApplication.java
@@ -0,0 +1,12 @@
+package com.ticketSolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsolderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ticketSolder/TicketsolderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ticketSolder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketsolderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
